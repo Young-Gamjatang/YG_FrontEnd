d7d30b56bef9846753eeb15645180b4202913422
ki-met-hoon <101192772+ki-met-hoon@users.noreply.github.com>
2023-05-18 11:35:50 +0900

    feat: Create QualityRestaurantModel Interface
    create QualityRestaurantModel Interface

--- frontEnd/app/src/main/java/com/example/test/Retrofitmanager/RetrofitService.java
+++ frontEnd/app/src/main/java/com/example/test/Retrofitmanager/RetrofitService.java
@@ -3,19 +3,41 @@ package com.example.test.Retrofitmanager;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
-import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface RetrofitService {
 
+    @GET("URL/{latitude}")
+    Call<QualityRestaurantModel> getLatitude(@Path("latitude") Double latitude);
 
-    @GET("resturant")//그룹 리스트 조회
-    Call<ReslistModel>reslist(@Body ResModel resModel);
+    @GET("URL/{longitude}")
+    Call<QualityRestaurantModel> getLongitude(@Path("longitude") Double longitude);
 
+    @GET("URL/{upsoSno}")
+    Call<QualityRestaurantModel> getUpsoSno(@Path("upsoSno") String upsoSno);
 
+    @GET("URL/{cggCode}")
+    Call<QualityRestaurantModel> getCggCode(@Path("cggCode") String cggCode);
 
-    @GET("push/{tokens}")
-    Call<String> insertToken(@Path("tokens") String tokens);
+    @GET("URL/{sntCobNm}")
+    Call<QualityRestaurantModel> getSntCobNm(@Path("sntCobNm") String sntCobNm);
+
+    @GET("URL/{upsoNm}")
+    Call<QualityRestaurantModel> getUpsoNm(@Path("upsoNm") String upsoNm);
+
+    @GET("URL/{sntUptaeNm}")
+    Call<QualityRestaurantModel> getSntUptaeNm(@Path("sntUptaeNm") String sntUptaeNm);
+
+    @GET("URL/{siteAddrRd}")
+    Call<QualityRestaurantModel> getSiteAddrRd(@Path("siteAddrRd") String siteAddrRd);
+
+    @GET("URL/{bdngJisgFlrNum}")
+    Call<QualityRestaurantModel> getBdngJisgFlrNum(@Path("bdngJisgFlrNum") String bdngJisgFlrNum);
+
+    @GET("URL/{bdngUnderFlrNum}")
+    Call<QualityRestaurantModel> getBdngUnderFlrNum(@Path("bdngUnderFlrNum") String bdngUnderFlrNum);
+
+    @GET("URL/{geEhYn}")
+    Call<QualityRestaurantModel> getGeEhYn(@Path("geEhYn") String geEhYn);
 
 }
