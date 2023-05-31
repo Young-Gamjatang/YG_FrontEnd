1daa4430888784225e55baff8ec415a594047019
moyeongmin <qa98589858@gmail.com>
2023-05-31 15:54:42 +0900

    feat: 5/31 15:52 taplayout 작업 끝 spot_list로 넘어감

---
+++ frontEnd/app/src/main/java/com/example/test/SpotList.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpotList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spot_list);
+    }
+}
